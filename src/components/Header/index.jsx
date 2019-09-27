54929917ca7a40976d646260d65f4c1cc754e461
Noah Waldner <noah.waldner@bkb.ch>
2019-09-27 14:51:21 +0200

    init

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.scss'
+
+export default function index() {
+    return (
+        <div className="common-header-wrapper">
+            <div className="common-header-logo">
+                bzfdgfzds
+            </div>
+            <div className="common-header-entries">
+                <div className="common-header-entry">
+                    Bla
+                </div>
+                <div className="common-header-entry">
+                    sdfsadf
+                </div>
+            </div>
+
+        </div>
+    )
+}
