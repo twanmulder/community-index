e03cf4ba0927405c4c6753e78d8d6f19fc9a5642
Noah Waldner <noah.waldner@bkb.ch>
2019-10-14 16:26:55 +0200

    added profile preview

--- src/components/ProfilePreview/index.jsx
+++ src/components/ProfilePreview/index.jsx
@@ -5,8 +5,11 @@ export default function Header() {
     return (
         <div className="profile_preview__wrapper">
             <div className="profile_preview__header"></div>
-            <div className="profile_preview__body"></div>
             <div className="profile_preview__picture"></div>
+            <div className="profile_preview__body">
+                <h4 className="profile_preview_pilot_name">Main Title</h4>
+                <p>Subtitle</p>
+            </div>
         </div>
     )
 }
