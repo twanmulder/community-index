0d8500a3ea10d9fb6c063dd66d293d02dd466b98
Leo Rotzler <leo.rotzler@bkb.ch>
2019-10-14 14:59:39 +0200

    Added react-helmet

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import NavBar from './components/NavBar'
 import Header from './components/Header'
 import ProfilePreview from './components/ProfilePreview'
 import Layout from './components/Layout'
+import Meta from './components/Meta'
 
 function App() {
   return (
     <Layout>
+      <Meta title="Home" />
       <div style={{ height: "150vh" }}>
         <ProfilePreview></ProfilePreview>
       </div>
