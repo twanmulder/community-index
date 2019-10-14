cec875367d0b26cf91e8edd89918c18040c5db61
Leo Rotzler <leo.rotzler@bkb.ch>
2019-10-14 15:19:18 +0200

    Added ProfileIcon component

---
+++ src/components/ProfileIcon/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './style.scss'
+
+export default function index() {
+    const image = 'https://i.pravatar.cc/150'
+    return (
+        <Link className="profile-icon" to="/profile">
+            <div className="profile-icon__image" style={{ backgroundImage: `url(${image})` }} />
+        </Link>
+    )
+}
