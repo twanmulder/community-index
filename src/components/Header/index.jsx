f1bdc986ee959bf1ef1f3a27b1837ef5688fb26c
ujjawaljaiswal2017 <ujjawaljaiswal2017@gmail.com>
2019-10-15 13:43:40 +0530

    Added Progressive image in Header function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react' 
 import './style.scss'
+import ProgressiveImage from 'react-progressive-image';
 
 export default function Header() {
+    
     return (
-        <div className="header__wrapper" style={{ backgroundImage: "url('https://source.unsplash.com/random')" }}>
+        // replace value of placeholder attribute with the location of Low-res image and the value of src with location of high-res image
+        //I purposely made the High-res image very large so that the effect can be observed
+        <ProgressiveImage src="https://images.unsplash.com/flagged/photo-1568619612917-c112de048ebc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=10800&fit=max" placeholder="https://images.unsplash.com/flagged/photo-1568619612917-c112de048ebc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=108&fit=max">
+            {src => 
+            <div className="header__wrapper" style={{ backgroundImage: "url('"+src+"')" }}>
 
-        </div>
+            </div>}
+        </ProgressiveImage>
+        
     )
 }
