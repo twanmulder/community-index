7e87a490b8465b5e32f36ee9a5e9978cb21eae2f
Pitchayut Chitsinpchayakun <pitchayut@oxygenai.com>
2019-10-16 13:12:18 +0700

    Add Apollo for GraphQL fetching & Integrate with Home Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import './App.css';
-import ProfilePreview from './components/ProfilePreview/ProfilePreview'
-import Layout from './components/Layout/Layout'
-import Meta from './components/Meta/Meta'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ProfilePreview from "./components/ProfilePreview/ProfilePreview";
+import Layout from "./components/Layout/Layout";
+import Meta from "./components/Meta/Meta";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const FETCH_USERS = gql`
+  query fetchUsers {
+    user_tbl {
+      full_name
+      username
+    }
+  }
+`;
 
 function App() {
+  const { data: usersData, error: usersDataError } = useQuery(FETCH_USERS);
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  useEffect(() => {
+    if (usersData && usersData.user_tbl) {
+      setUserProfiles(usersData.user_tbl);
+    }
+  }, [usersData]);
+
+  if (usersDataError) alert("Error when loading users data.");
+
   return (
     <Layout>
       <Meta title="Home" />
       <div style={{ height: "150vh" }}>
-        <ProfilePreview></ProfilePreview>
+        {userProfiles &&
+          userProfiles.map(userData => (
+            <ProfilePreview
+              full_name={userData.full_name}
+              username={userData.username}
+            />
+          ))}
       </div>
     </Layout>
   );
